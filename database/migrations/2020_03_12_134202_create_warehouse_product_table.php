33ff670d3a93cf88c64e9027d13eabeaa6e5ccac
Chloé Vieille <chloevieille@hotmail.com>
2020-03-12 14:47:27 +0100

    Creation de la base de donnée

---
+++ database/migrations/2020_03_12_134202_create_warehouse_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWarehouseProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('warehouse_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products');
+            $table->foreign('warehouse_id')
+                ->references('id')
+                ->on('warehouses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('warehouse_product');
+    }
+}
